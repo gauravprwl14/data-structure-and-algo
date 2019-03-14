9a379aa941b5d3e412e6df51ae7b2e70d425e50e
gauravprwl14 <gauravprwl14@gmail.com>
2019-03-14 22:57:34 +0530

    added the function to create the binary tree

---
+++ tree/binaryTree/create-binary-tree.js
@@ -0,0 +1,44 @@
+function Node(val){
+    this.value = val;
+    this.left = null;
+    this.right = null;
+  }
+
+  function BinarySearchTree(){
+    this.root = null;
+  }
+  
+  
+  BinarySearchTree.prototype.push = function(val){
+    var root = this.root;
+ 
+    if(!root){
+       this.root = new Node(val);
+       return;
+    }
+ 
+    var currentNode = root;
+    var newNode = new Node(val); 
+ 
+    while(currentNode){
+       if(val < currentNode.value){
+           if(!currentNode.left){
+              currentNode.left = newNode;
+              break;
+           }
+           else{
+              currentNode = currentNode.left;
+         }
+      }
+      else{
+          if(!currentNode.right){
+             currentNode.right = newNode;
+             break;
+          }
+          else{
+             currentNode = currentNode.right;
+          }
+      }
+   }
+ 
+ }
